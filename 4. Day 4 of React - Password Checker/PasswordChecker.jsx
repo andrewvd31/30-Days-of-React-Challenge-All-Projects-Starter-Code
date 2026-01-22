3e28d8cb59dcbbbbb7fdb166703be50e29ae70db
Andrew V D <andrewvd31@gmail.com>
2026-01-22 21:31:13 +0530

    Day 4 Project Updated

---
+++ 4. Day 4 of React - Password Checker/PasswordChecker.jsx
@@ -0,0 +1,46 @@
+const styles = {
+  page: {
+    minHeight: "100vh",
+    backgroundColor: "#00B7B5",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "40px",
+  },
+
+  card: {
+    width: "460px",
+    backgroundColor: "rgba(255,255,255,0.95)",
+    borderRadius: "24px",
+    padding: "40px",   
+    boxShadow: "0 20px 20px rgba(0,0,0,0.1)",
+  },
+
+  title: {
+    fontSize: "28px",
+    fontWeight: 700,
+    marginBottom: "8px",
+  },
+
+  subtitle: {
+    fontSize: "15px",
+    color: "#475569",
+    marginBottom: "28px",
+  },
+
+  input: {
+    width: "100%",
+    padding: "16px",
+    borderRadius: "10px",
+    border: "1px solid #070027",
+    fontSize: "15px",
+    marginBottom: "24px",
+    outline: "none"
+  },
+
+  result: {
+    display: "flex",
+    justifyContent: "space-between",
+    fontSize: "14px",
+  },
+};
